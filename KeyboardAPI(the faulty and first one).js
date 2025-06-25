44d594aa03c971b95c50a3800020effc8be10906
matthewye-061612 <matthewye.tp@gmail.com>
2025-06-24 21:09:48 -0700

    Add files via upload

---
+++ KeyboardAPI(the faulty and first one).js
@@ -0,0 +1,166 @@
+var x = 0;
+var y = 0;
+function draw() {
+  if(onkeydown("`")) {
+    text("`", x+10, y);
+  }
+  if(onkeydown("`") && onkeydown("shift")) {
+    text("~", x+10, y);
+  }
+  if (onkeydown("q")) {
+    text("q", x+20, y);
+  }
+  if(onkeydown("q") && onkeydown("shift")) {
+    text("Q", x+20, y);
+  }
+  if(onkeydown("w")) {
+    text("w", x+30, y);
+  }
+  if(onkeydown("w") && onkeydown("shift")) {
+    text("W", x+30, y);
+  }
+  if(onkeydown("e")) {
+    text("e", x+40, y);
+  }
+  if(onkeydown("e") && onkeydown("shift")) {
+    text("E", x+40, y);
+  }
+  if(onkeydown("r")) {
+    text("r", x+50, y);
+  }
+  if(onkeydown("r") && onkeydown("shift")) {
+    text("R", x+50, y);
+  }
+  if(onkeydown("t")) {
+    text("t", x+60, y);
+  }
+  if(onkeydown("t") && onkeydown("shift")) {
+    text("T", x+60, y);
+  }
+  if(onkeydown("y")) {
+    text("y", x+70, y);
+  }
+  if(onkeydown("y") && onkeydown("shift")) {
+    text("Y", x+70, y);
+  }
+  if(onkeydown("u")) {
+    text("u", x+80, y);
+  }
+  if(onkeydown("u") && onkeydown("shift")) {
+    text("U", x+80, y);
+  }
+  if(onkeydown("i")) {
+    text("i", x+90, y);
+  }
+  if(onkeydown("i") && onkeydown("shift")) {
+    text("I", x+90, y);
+  }
+  if(onkeydown("o")) {
+    text("o", x+100, y);
+  }
+  if(onkeydown("o") && onkeydown("shift")) {
+    text("O", x+100, y);
+  }
+  if(onkeydown("p")) {
+    text("p", x+110, y);
+  }
+  if(onkeydown("p") && onkeydown("shift")) {
+    text("P", x+110, y);
+  }
+  if(onkeydown("[")) {
+    text("[", x+120, y);
+  }
+  if(onkeydown("[") && onkeydown("shift")) {
+    text("{", x+120, y);
+  }
+  if(onkeydown("]")) {
+    text("]", x+130, y);
+  }
+  if(onkeydown("]") && onkeydown("shift")) {
+    text("}", x+130, y);
+  }
+  if(onkeydown("\")) {
+    text("\", x+140, y);
+  }
+  if(onkeydown("\") && onkeydown("shift")) {
+    text("|", x+140, y);
+  }
+  if(onkeydown("-")) {
+    text("-", x+140, y);
+  }
+  if(onkeydown("-") && onkeydown("shift")) {
+    text("_", x+140, y);
+  }
+  if(onkeydown("=")) {
+    text("=", x+150, y);
+  }
+  if(onkeydown("+") && onkeydown("shift")) {
+    text("+", x+150, y);
+  }
+  if(onkeydown("a")) {
+    text("a", x+160, y);
+  }
+  if(onkeydown("a") && onkeydown("shift")) {
+    text("A", x+160, y);
+  }
+  if(onkeydown("s")) {
+    text("s", x+170, y);
+  }
+  if(onkeydown("s") && onkeydown("shift")) {
+    text("S", x+170, y);
+  }
+  if(onkeydown("d")) {
+    text("d", x+180, y);
+  }
+  if(onkeydown("d") && onkeydown("shift")) {
+    text("D", x+180, y);
+  }
+  if(onkeydown("f")) {
+    text("f", x+190, y);
+  }
+  if(onkeydown("f") && onkeydown("shift")) {
+    text("F", x+190, y);
+  }
+  if(onkeydown("g")) {
+    text("g", x+200, y);
+  }
+  if(onkeydown("g") && onkeydown("shift")) {
+    text("G", x+200, y);
+  }
+  if(onkeydown("h")) {
+    text("h", x+210, y);
+  }
+  if(onkeydown("h") && onkeydown("shift")) {
+    text("H", x+210, y);
+  }
+  if(onkeydown("j")) {
+    text("j", x+220, y);
+  }
+  if(onkeydown("j") && onkeydown("shift")) {
+    text("J", x+220, y);
+  }
+  if(onkeydown("k")) {
+    text("k", x+230, y);
+  }
+  if(onkeydown("k") && onkeydown("shift")) {
+    text("K", x+230, y);
+  }
+  if(onkeydown("l")) {
+    text("l", x+240, y);
+  }
+  if(onkeydown("l") && onkeydown("shift")) {
+    text("L", x+240, y);
+  }
+  if(onkeydown("e")) {
+    text("e", x+250, y);
+  }
+  if(onkeydown("e") && onkeydown("shift")) {
+    text("E", x+250, y);
+  }
+
+
+
+
+}
+
+console.log(draw);
